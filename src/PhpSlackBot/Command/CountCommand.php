2bcfefc72001b49ed058716071bc7e08d2f7edd4
jc <jeancharleslegoff@gmail.com>
2015-10-20 20:55:53 +0200

    Add "count" command as example command

---
+++ src/PhpSlackBot/Command/CountCommand.php
@@ -0,0 +1,18 @@
+<?php
+namespace PhpSlackBot\Command;
+
+class CountCommand extends BaseCommand {
+
+    private $count = 0;
+
+    protected function configure() {
+        $this->setName('count');
+    }
+
+    protected function execute($message, $context) {
+        $this->send($this->getCurrentChannel(), null, $this->count);
+        $this->count++;
+        return $response;
+    }
+
+}
